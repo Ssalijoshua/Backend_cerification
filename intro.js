02a643c9652cd77eaa955c69c9f2940a70b0c243
Ssalijoshua <140295781+Ssalijoshua@users.noreply.github.com>
2026-03-14 20:27:32 +0000

    submitted assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+
+const name = "Ssali Joshua";
+const age =  23;
+const favouriteProgrammingLanguage = "Python";
+
+// Log information to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favouriteProgrammingLanguage);
